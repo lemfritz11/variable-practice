12ed42c2a80a20a7bff270f3cbf74979b069cffe
lemfritz11 <aintdaiki@gmail.com>
2024-09-13 21:51:10 +0800

    initial commit with variable.js

---
+++ variable.js
@@ -0,0 +1,9 @@
+let name = "Lem Fritz";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Male";
+let twitterHandle = "@aintdaiki111104";
+
+console.log("Name:", name);
+console.log("What do you want to become in your life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
